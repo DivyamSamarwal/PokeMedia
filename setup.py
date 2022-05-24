45e7f247efbcb1d0ae0bb3a0a2c28663cb8cd133
Divyam Samarwal <divyamsamarwal@gmail.com>
2022-05-24 14:53:34 +0530

    updates

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-setup(
-    name='pokemedia',
-    version='1.0.1',
-    description='A Pokedex',
-    author='Divyam Samarwal',
-    license='MIT',
-    packages=['Pokemon_Data']
-)
